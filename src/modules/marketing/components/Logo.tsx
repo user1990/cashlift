6c3b5fa339f9c124c9102ee515fc9569b13ea5b8
user1990 <paulius.rimg1990@gmail.com>
2026-06-26 15:37:29 +0300

    Brand: Reduce CashLift logo size

--- src/modules/marketing/components/Logo.tsx
+++ src/modules/marketing/components/Logo.tsx
@@ -4,6 +4,6 @@ import logo from "@/app/logo.svg";
 
 export const Logo = () => (
 	<Link href="/" className="shrink-0">
-		<Image src={logo} alt="CashLift Logo" width={186} height={36} priority className="h-9 w-auto shrink-0" />
+		<Image src={logo} alt="CashLift Logo" width={166} height={32} priority className="h-8 w-auto shrink-0" />
 	</Link>
 );
