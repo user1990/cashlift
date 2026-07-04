6d1c6145a414dc8e4ae91e0f1f9da6843424f5c3
user1990 <paulius.rimg1990@gmail.com>
2026-07-04 16:30:01 +0300

    Allow dev eval in home CSP

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const HOME_CONTENT_SECURITY_POLICY = [
 	"default-src 'self'",
-	"script-src 'self' 'unsafe-inline'",
+	`script-src 'self' 'unsafe-inline'${process.env.NODE_ENV === "development" ? " 'unsafe-eval'" : ""}`,
 	"style-src 'self' 'unsafe-inline'",
 	"style-src-attr 'none'",
 	"img-src 'self' blob: data: https:",
