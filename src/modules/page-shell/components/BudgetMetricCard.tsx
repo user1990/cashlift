a5d942a37d22ab7a6c1dd2567f7b6310cac7da53
user1990 <paulius.rimg1990@gmail.com>
2026-07-23 15:59:54 +0300

    Fix: Reorder import statement for formatCurrency in BudgetMetricCard component

--- src/modules/page-shell/components/BudgetMetricCard.tsx
+++ src/modules/page-shell/components/BudgetMetricCard.tsx
@@ -1,5 +1,5 @@
-import { formatCurrency } from "@/modules/money/format";
 import { getRemainingTeamBudget } from "@/modules/budgets/utils";
+import { formatCurrency } from "@/modules/money/format";
 import { WorkspaceMetricCard } from "./WorkspaceMetricCard";
 
 type BudgetMetricCardProps = {
