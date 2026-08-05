a225e464f9d87d92ee977945a0d0af2c7cc23b55
user1990 <paulius.rimg1990@gmail.com>
2026-08-05 13:19:59 +0300

    Marketing: Preserve final sticky runway

--- src/modules/marketing/components/HomeDecisionStorySection.tsx
+++ src/modules/marketing/components/HomeDecisionStorySection.tsx
@@ -16,7 +16,7 @@ export const HomeDecisionStorySection = ({ actions }: HomeDecisionStorySectionPr
 		</header>
 
 		<div className="mx-auto max-w-295 px-4 pb-20 sm:px-6 lg:px-8 lg:pb-28">
-			<ol className="lg:pb-[16svh]">
+			<ol>
 				{actions.map((action) => {
 					const chapter = DECISION_CHAPTERS[action.type];
 
@@ -29,6 +29,8 @@ export const HomeDecisionStorySection = ({ actions }: HomeDecisionStorySectionPr
 						/>
 					);
 				})}
+
+				<li aria-hidden data-story-trailing-space="true" className="hidden h-[16svh] lg:block" />
 			</ol>
 		</div>
 	</section>
